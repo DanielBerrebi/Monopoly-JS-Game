818c663804c6bc698373a9c331d031d947863e83
DanielBerrebi <berrebidaniel1@gmail.com>
2022-07-17 13:35:25 +0300

    upload

---
+++ board.js
@@ -0,0 +1,200 @@
+﻿cards = {
+    "concept": {
+        "ElectricShop": [
+            {
+                "genre": "lighting system",
+                "name": "Flurosent",
+                "price": 250,
+                "image": "Flurosent.jpg"
+            },
+            {
+                "genre": "lighting system",
+                "name": "Led",
+                "price": 300,
+                "image": "Led.jpg"
+            },
+            {
+                "genre": "vacuum cleaners",
+                "name": "Xiaomi XR",
+                "price": 350,
+                "image": "Xiaomi.jpg"
+            },
+            {
+                "genre": "vacuum cleaners",
+                "name": "Bosch BCS",
+                "price": 400,
+                "image": "Bosch.jpg"
+            },
+            {
+                "genre": "washing machines",
+                "name": "Midea MFWS",
+                "price": 550,
+                "image": "Midea.jpg"
+            },
+            {
+                "genre": "washing machines",
+                "name": "AEG L8FSC",
+                "price": 600,
+                "image": "AEG.jpg"
+            },
+            {
+                "genre": "Dishwashers",
+                "name": "Beko DFN",
+                "price": 750,
+                "image": "Beko.jpg"
+            },
+            {
+                "genre": "Dishwashers",
+                "name": "Crystal DW",
+                "price": 800,
+                "image": "Crystal.jpg"
+            },
+            {
+                "genre": "air conditioners",
+                "name": "Amcor 21",
+                "price": 950,
+                "image": "Amcor.jpg"
+            },
+            {
+                "genre": "air conditioners",
+                "name": "Tornado TOP",
+                "price": 1000,
+                "image": "Tornado.jpg"
+            },
+            {
+                "genre": "refrigerators",
+                "name": "LG LFXC",
+                "price": 1150,
+                "image": "LG.jpg"
+            },
+            {
+                "genre": "refrigerators",
+                "name": "Siemens KA",
+                "price": 1200,
+                "image": "Simenes.jpg"
+            },
+            {
+                "genre": "ovens",
+                "name": "Sol HO",
+                "price": 1350,
+                "image": "Sol.jpg"
+            },
+            {
+                "genre": "ovens",
+                "name": "Sauter SFE",
+                "price": 1400,
+                "image": "Sauter.jpg"
+            },
+            {
+                "genre": "cellphones",
+                "name": "Samsung galaxy",
+                "price": 1500,
+                "image": "Samsung.jpg"
+            },
+            {
+                "genre": "cellphones",
+                "name": "Iphone 12",
+                "price": 1550,
+                "image": "Iphone.jpg"
+            }
+        ],
+        "Movies": [
+            {
+                "genre": "Action",
+                "name": "Wonderwoman 1984",
+                "price": 950,
+                "image": "Wonderwoman.jpg"
+            },
+            {
+                "genre": "Action",
+                "name": "The Suicide Squad",
+                "price": 700,
+                "image": "TheSuicideSquad.jpeg"
+            },
+            {
+                "genre": "Comedy",
+                "name": "Tom&Jerry",
+                "price": 80,
+                "image": "Tom&Jerry.JFIF"
+            },
+            {
+                "genre": "Comedy",
+                "name": "Scooby-Doo",
+                "price": 55,
+                "image": "Scooby-Doo.jpg"
+            },
+            {
+                "genre": "Drama",
+                "name": "The Judge",
+                "price": 250,
+                "image": "TheJudge.jpg"
+            },
+            {
+                "genre": "Drama",
+                "name": "American Sniper",
+                "price": 300,
+                "image": "AmericanSniper.jpg"
+            },
+            {
+                "genre": "Science fiction",
+                "name": "Matrix",
+                "price": 400,
+                "image": "Matrix.jpg"
+            },
+            {
+                "genre": "Science fiction",
+                "name": "Inception",
+                "price": 300,
+                "image": "Inception.jfif"
+            },
+            {
+                "genre": "Horror",
+                "name": "The Conjuring",
+                "price": 200,
+                "image": "Conjuring.jfif"
+            },
+            {
+                "genre": "Horror",
+                "name": "Annabelle",
+                "price": 350,
+                "image": "Annabelle.jpg"
+            },
+            {
+                "genre": "Adventure",
+                "name": "The Hobbit",
+                "price": 280,
+                "image": "TheHobbit.jpg"
+            },
+            {
+                "genre": "Adventure",
+                "name": "Tarzan",
+                "price": 180,
+                "image": "Tarzan.jpg"
+            },
+            {
+                "genre": "Romance",
+                "name": "casabllanca",
+                "price": 100,
+                "image": "casabllanca.jpg"
+            },
+            {
+                "genre": "Romance",
+                "name": "Isn't It Romantic",
+                "price": 50,
+                "image": "Isn'tItRomantic.jpg"
+            },
+            {
+                "genre": "War",
+                "name": "Gladiator",
+                "price": 760,
+                "image": "Gladiator.jpg"
+            },
+            {
+                "genre": "War",
+                "name": "300",
+                "price": 870,
+                "image": "300.jpg"
+            }
+        ]
+    }
+}
